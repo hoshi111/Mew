a6ca4a8edae18c7b35462cb5e6a06891803bdf7b
Jason Villanueva <villanueva.jason2106@gmail.com>
2024-06-14 18:06:00 +0800

    wip

--- src/app/components/download-modal/download-modal.component.ts
+++ src/app/components/download-modal/download-modal.component.ts
@@ -16,43 +16,66 @@ export class DownloadModalComponent  implements OnInit {
   current = 0;
   max = 0;
   interval: any;
+  icons: any = [];
   iconName = 'arrow-down-circle';
+  downloadListener: any;
+  download: any;
   
   constructor(
               private apiService: ApiService
   ) { }
 
   ngOnInit() {
-    Filesystem.removeAllListeners();
-    
-    // 
-    // console.log(this.progress);
+    for (let i = 0; i < this.state.length; i++) {
+      this.icons[i] = 'arrow-down-circle';
+    }
   }
 
-  downloadEpisode(value: any) {
-    this.progress = document.getElementById('progress') as HTMLDivElement;
-    this.progress.classList.remove("progressHidden");
-    this.iconName = 'close-circle';
-    
-    this.playVideo(value.id).then((result: any) => {
-      this.getDownloadLink(result.download).then((links: any) => {
+  downloadEpisode(value: any, index: any) {
+    let link = '';
+    if (this.icons[index] == 'arrow-down-circle') {
+      this.progress = document.getElementById('progress') as HTMLDivElement;
+      this.progress.classList.remove("progressHidden");
+      this.icons[index] = 'close-circle';
+      
+      this.playVideo(value.id).then((result: any) => {
+        this.getDownloadLink(result.download).then((links: any) => {
+          console.log(links)
+          link = links[links.length-1].link;
+          this.downloadListener = Filesystem.addListener("progress", data => {
+            console.log(data.bytes + '/' + data.contentLength)
+            this.current = this.current + data.bytes;
+            this.max = data.contentLength;
+          })
 
-        Filesystem.addListener("progress", data => {
-          console.log(data.bytes + '/' + data.contentLength)
-          this.current = data.bytes;
-          this.max = data.contentLength;
+          this.download = Filesystem.downloadFile({
+            url: link,
+            path: value.id + '.mp4',
+            directory: Directory.Library,
+            progress: true,
+          }).then((res: DownloadFileResult) => {
+            this.progress.classList.add("progressHidden");
+            this.current = 0;
+            this.max = 0;
+            this.icons[index] = 'checkmark-circle';
+            console.log(res)
+          });
         })
-
-        Filesystem.downloadFile({
-          url: links[links.length-1].link,
-          path: value.id + '.mp4',
-          directory: Directory.Library,
-          progress: true,
-        }).then((res: DownloadFileResult) => {
-          console.log(res)
-        });
       })
+    }
+
+  else if (this.icons[index] == 'close-circle') {
+    console.log(link)
+    this.progress = document.getElementById('progress') as HTMLDivElement;
+    this.progress.classList.add("progressHidden");
+    this.icons[index] = 'arrow-down-circle';
+    Filesystem.deleteFile({
+      path: link,
+      directory: Directory.Library
     })
+    this.downloadListener.remove();
+  }
+    
     
   }
 
